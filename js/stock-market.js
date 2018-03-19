65a6fe6970b4e3617edc6b5501ff2cdf14093595
Abdullah Haneef <abddagia@gmail.com>
2018-03-20 04:16:18 +0500

    updated with for loop

--- js/stock-market.js
+++ js/stock-market.js
@@ -18,130 +18,77 @@ function showPosition(position) {
     lon = position.coords.longitude;
 
 
+    for(i=0;i<3;i++){
     //myURL = "https://fcc-weather-api.glitch.me/api/current?lon=" + lon + "&lat=" + lat;
-    msftStockInfo = "https://api.iextrading.com/1.0/stock/msft/batch?types=quote";
-    msftstockLogo = "https://api.iextrading.com/1.0/stock/msft/logo";
- 
-    appleStockInfo = "https://api.iextrading.com/1.0/stock/aapl/batch?types=quote";
-    applestockLogo = "https://api.iextrading.com/1.0/stock/aapl/logo";
+        console.log(i)
+        if (i == 0){
+            StockInfo = "https://api.iextrading.com/1.0/stock/msft/batch?types=quote";
+            stockLogo = "https://api.iextrading.com/1.0/stock/msft/logo";
 
-    ebayStockInfo = "https://api.iextrading.com/1.0/stock/ebay/batch?types=quote";
-    ebaystockLogo = "https://api.iextrading.com/1.0/stock/ebay/logo";     
-
-
-
-// Microsoft Stock Logo
-    $.get(msftstockLogo, function (data) {
-        $(".logo_msft").attr('src', data.url);
-    });
-
-// Microsoft Stock Info
-    $.get(msftStockInfo, function (data) {
-
-        //var symbol = document.getElementsByClassName("symbol");
-        //symbol[0].innerHTML = data.quote.symbol + " - ";
-        $(".symbol_msft").html(data.quote.symbol + " - ");
-        $(".companyName_msft").html(data.quote.companyName);
-        $(".sector_msft").html(data.quote.sector);
-        $(".latestPrice_msft").html(data.quote.latestPrice);
-
-
-        // Change to green or red based on the price change
-        var change = data.quote.change;
-        if (change > 0) {
-            document.getElementById('change_msft').innerHTML = "+" + change;
-        } else {
-            document.getElementById('change_msft').innerHTML = "" + change;
-            var elementGreen = document.getElementById('change_msft');
-            elementGreen.className -= " btn-success";
-            elementGreen.className += " btn-danger";
         }
-
-        // My Private number of shares
-        var numberShares = 9;
-        var price = data.quote.latestPrice;
-        var total = Math.round(numberShares * price);
-        var numberSharesV = document.getElementsByClassName('numberShares_msft');
-        numberSharesV[0].innerHTML = "Number of Shares: " + numberShares;
-        var totalPriceV = document.getElementsByClassName('totalPrice_msft');
-        totalPriceV[0].innerHTML = total;
-    });
-
-// apple Stock Logo
-    $.get(applestockLogo, function (data) {
-        $(".logo_apple").attr('src', data.url);
-    });
-
-// apple Stock Info
-    $.get(appleStockInfo, function (data) {
-
-        //var symbol = document.getElementsByClassName("symbol");
-        //symbol[0].innerHTML = data.quote.symbol + " - ";
-        $(".symbol_apple").html(data.quote.symbol + " - ");
-        $(".companyName_apple").html(data.quote.companyName);
-        $(".sector_apple").html(data.quote.sector);
-        $(".latestPrice_apple").html(data.quote.latestPrice);
-
-
-        // Change to green or red based on the price change
-        var change = data.quote.change;
-        if (change > 0) {
-            document.getElementById('change_apple').innerHTML = "+" + change;
-        } else {
-            document.getElementById('change_apple').innerHTML = "" + change;
-            var elementGreen = document.getElementById('change_apple');
-            elementGreen.className -= " btn-success";
-            elementGreen.className += " btn-danger";
+        else if(i == 1){     
+            StockInfo = "https://api.iextrading.com/1.0/stock/aapl/batch?types=quote";
+            stockLogo = "https://api.iextrading.com/1.0/stock/aapl/logo";
         }
+        else{
+            StockInfo = "https://api.iextrading.com/1.0/stock/ebay/batch?types=quote";
+            stockLogo = "https://api.iextrading.com/1.0/stock/ebay/logo";     
+        };
 
-        // My Private number of shares
-        var numberShares = 9;
-        var price = data.quote.latestPrice;
-        var total = Math.round(numberShares * price);
-        var numberSharesV = document.getElementsByClassName('numberShares_apple');
-        numberSharesV[0].innerHTML = "Number of Shares: " + numberShares;
-        var totalPriceV = document.getElementsByClassName('totalPrice_apple');
-        totalPriceV[0].innerHTML = total;
-
-    });
-
-// ebay Stock Logo
-    $.get(ebaystockLogo, function (data) {
-        $(".logo_ebay").attr('src', data.url);
-    });
-
-// ebay Stock Info
-    $.get(ebayStockInfo, function (data) {
-
-        //var symbol = document.getElementsByClassName("symbol");
-        //symbol[0].innerHTML = data.quote.symbol + " - ";
-        $(".symbol_ebay").html(data.quote.symbol + " - ");
-        $(".companyName_ebay").html(data.quote.companyName);
-        $(".sector_ebay").html(data.quote.sector);
-        $(".latestPrice_ebay").html(data.quote.latestPrice);
+        $.ajax({
+            url: StockInfo,
+            type: 'GET',
+            beforeSend: function() {
+                console.log("Downloading ");
+            },
+            async: false,
+            complete: function() {
+            },
+            success: function(data) {
+                console.log(data);
+                $(".logo_"+i).attr('src', data.url);
+                $(".symbol_"+i).html(data.quote.symbol + " - ");
+                $(".companyName_"+i).html(data.quote.companyName);
+                $(".sector_"+i).html(data.quote.sector);
+                $(".latestPrice_"+i).html(data.quote.latestPrice);
 
 
-        // Change to green or red based on the price change
-        var change = data.quote.change;
-        if (change > 0) {
-            document.getElementById('change_ebay').innerHTML = "+" + change;
-        } else {
-            document.getElementById('change_ebay').innerHTML = "" + change;
-            var elementGreen = document.getElementById('change_ebay');
-            elementGreen.className -= " btn-success";
-            elementGreen.className += " btn-danger";
-        }
+                // Change to green or red based on the price change
+                var change = data.quote.change;
+                if (change > 0) {
+                    document.getElementById('change_'+i).innerHTML = "+" + change;
+                } else {
+                    document.getElementById('change_'+i).innerHTML = "" + change;
+                    var elementGreen = document.getElementById('change_'+i);
+                    elementGreen.className -= " btn-success";
+                    elementGreen.className += " btn-danger";
+                }
 
-        // My Private number of shares
-        var numberShares = 9;
-        var price = data.quote.latestPrice;
-        var total = Math.round(numberShares * price);
-        var numberSharesV = document.getElementsByClassName('numberShares_ebay');
-        numberSharesV[0].innerHTML = "Number of Shares: " + numberShares;
-        var totalPriceV = document.getElementsByClassName('totalPrice_ebay');
-        totalPriceV[0].innerHTML = total;
+                // My Private number of shares
+                var numberShares = 9;
+                var price = data.quote.latestPrice;
+                var total = Math.round(numberShares * price);
+                var numberSharesV = document.getElementsByClassName('numberShares_'+i);
+                numberSharesV[0].innerHTML = "Number of Shares: " + numberShares;
+                var totalPriceV = document.getElementsByClassName('totalPrice_'+i);
+                totalPriceV[0].innerHTML = total;                
+            }
+        });
 
-    });    
+        $.ajax({
+            url: stockLogo,
+            type: 'GET',
+            beforeSend: function() {
+                console.log("Downloading ");
+            },
+            async: false,
+            complete: function() {
+            },
+            success: function(data_logo) {
+                $(".logo_"+i).attr('src', data_logo.url);
 
+            }
+        });
+    };
 };
 
